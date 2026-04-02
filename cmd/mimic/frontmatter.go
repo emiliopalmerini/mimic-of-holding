b2c84dadf8b3bb34c9844337be2b4922a80a6ce7
emiliopalmerini <emilio.palmerini@gmail.com>
2026-04-02 12:35:22 +0200

    refactor(vault): remove write/edit/append, add frontmatter CLI
    Remove write, edit, append commands and domain functions; agents use
    resolve + native file tools instead. Add frontmatter CLI command.
    Remove obsolete ADR-0032 (write stdin/editor).

---
+++ cmd/mimic/frontmatter.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/epalmerini/mimic-of-holding/internal/vault"
+	"github.com/spf13/cobra"
+)
+
+func newFrontmatterCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "frontmatter <action> <ref> <file> <key> <value>",
+		Short: "Edit YAML frontmatter fields",
+		Long:  "Edit YAML frontmatter in a file within a JD ID folder.\nActions: set (scalar), add (append to list), remove (remove from list).",
+		Args:  cobra.ExactArgs(5),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			v, err := parseVault()
+			if err != nil {
+				return err
+			}
+			action, ref, file, key, value := args[0], args[1], args[2], args[3], args[4]
+			var path string
+			switch action {
+			case "set":
+				path, err = vault.SetFrontmatter(v, ref, file, key, value)
+			case "add":
+				path, err = vault.AddToFrontmatterList(v, ref, file, key, value)
+			case "remove":
+				path, err = vault.RemoveFromFrontmatterList(v, ref, file, key, value)
+			default:
+				return fmt.Errorf("unknown action %q (use set, add, or remove)", action)
+			}
+			if err != nil {
+				return err
+			}
+			fmt.Fprintf(cmd.OutOrStdout(), "Updated frontmatter in %s\n", path)
+			return nil
+		},
+	}
+	return cmd
+}
